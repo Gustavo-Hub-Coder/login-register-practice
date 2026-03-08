77081ad67768e4713962931922fffbc3efbb045f
Gary_garcia <garciapino.gus@gmail.com>
2026-03-08 13:49:28 -0300

    update

---
+++ assets/js/logIn.js
@@ -0,0 +1,42 @@
+const userName2 = document.querySelector("#input-username2");
+const password2 = document.querySelector("#input-password2");
+const loginBtn = document.querySelector("#login-btn");
+const form = document.querySelector(".container");
+const deleteAccountsBtn = document.querySelector("#delete-accounts-btn");
+
+
+//Para prevenir el evento de enviar
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log("event prevent");
+})
+
+loginBtn.addEventListener("click", function() {
+    if (!userName2.value || !password2.value) {
+      return alert("Please fill in all fields");  
+    } 
+
+    //recuperar lista de users del localstorage
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    //buscar usuario com el mismo nombre y contraseña
+    const userValidate = users.find(u =>
+        u.userName === userName2.value && u.password === password2.value
+    );
+
+    if (userValidate) {
+        alert("login successful");
+        //aqui puedo redirecionar para otra pagina
+    } else {
+        alert("Invalid username or password");
+    }
+})
+
+deleteAccountsBtn.addEventListener('click', function() {
+    const confirmDelete = confirm("Are you sure you want to delete all accounts?");
+
+    if (confirmDelete) {
+        localStorage.removeItem("users");
+        alert("All accounts were removed");
+    }
+});
---
+++ assets/js/signUp.js
@@ -0,0 +1,70 @@
+const userName = document.querySelector("#input-username");
+const email = document.querySelector("#input-email");
+const password = document.querySelector("#input-password");
+const confirmPassword = document.querySelector("#input-confirm-password");
+const signUpBtn = document.querySelector("#signup-btn");
+const form = document.querySelector(".container");
+const deleteAccountsBtn = document.querySelector("#delete-accounts-btn");
+
+//crear cuentas nuevas
+function createAccount() {
+    const newUser = {
+    userName: userName.value,
+    email: email.value,
+    password: password.value,
+    confirmPassword: confirmPassword.value,
+    }
+    //leer usuarios guardados
+    //users variavle donde se guardará la lista de usuarios
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    //agregar el nuevo usuario
+    users.push(newUser);
+    //guardar los datos en el localstorage
+    localStorage.setItem("users", JSON.stringify(users));
+};
+
+//Para prevenir el evento de enviar
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log("event prevent")
+})
+
+//validar inputs
+function validateInputs() {
+    if (!userName.value) return false, alert("Username is required");
+    if (!email.value) return false, alert("Email is required");
+    if (!password.value) return false, alert("Password is required");
+    if (!confirmPassword.value) return false, alert("Please confirm your password");
+
+    if (password.value !== confirmPassword.value){
+        alert("Password do not match");
+        return false;
+    }
+    return true;
+}
+
+//limpiar inputs cuando se crea la cuenta
+function clearInputs() {
+    userName.value = "";
+    email.value = "";
+    password.value = "";
+    confirmPassword.value = "";
+}
+
+//verificando si se captura los valores delos inputs
+//click de boton
+signUpBtn.addEventListener("click", function() {
+    if (!validateInputs()) return;
+    createAccount();
+    clearInputs();
+    alert("account created.")
+});
+
+deleteAccountsBtn.addEventListener('click', function() {
+    const confirmDelete = confirm("Are you sure you want to delete all accounts?");
+    console.log("clicked")
+    if (confirmDelete) {
+        localStorage.removeItem("users");
+        alert("All accounts were removed");
+    }
+});
